d4c49db9f6cc3e500f0d9866bc52b6d7ddfc8282
nlchar <contact.me@nlchar.rocks>
2021-07-25 18:14:58 +0300

    added compilation test support

---
+++ .github/workflows/eo-language-tests/python/run-test.py
@@ -0,0 +1,35 @@
+import sys, os, shutil, subprocess
+import json
+import glob
+
+test_dir = sys.argv[1]
+path = ".github/workflows/eo-language-tests/tests/"
+
+
+with open(path + test_dir + "/test.json", 'r') as test_data_file:
+  test_data = json.load(test_data_file)
+
+
+if test_data["type"] == "compilation":
+  dest_dir = ".github/workflows/eo-language-tests/environments/compilation-tests-environment"
+else:
+  raise TypeError("Uknown type of the test!")
+
+for filename in glob.glob(os.path.join(path + test_dir, '*.*')):
+  if os.path.isfile(filename):
+    shutil.copy(filename, dest_dir + "/eo")
+
+
+completed_compilation_process = subprocess.run(["mvn", "clean", "compile", "--file", dest_dir + '/pom.xml'])
+
+if completed_compilation_process.returncode == 0:
+  actual_compilation_result = "ok"
+else:
+  actual_compilation_result = "fail"
+
+expected_compilation_result = test_data["result"]
+
+if expected_compilation_result != actual_compilation_result:
+  raise AssertionError("Expected compilation result: \"" + expected_compilation_result + "\", but got: \"" + actual_compilation_result + "\".")
+else:
+  print("OK!")
